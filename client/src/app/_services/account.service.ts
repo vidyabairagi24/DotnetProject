175ba5a904e3b6e006a6da09aa5a696b2fb823b8
vidya <vidyabairagi24@gmail.com>
2023-12-02 08:55:12 +0530

    End of Section 11

--- client/src/app/_services/account.service.ts
+++ client/src/app/_services/account.service.ts
@@ -20,8 +20,7 @@ export class AccountService {
       map((response: User) => {
         const user = response;
         if (user) {
-          localStorage.setItem('user', JSON.stringify(user))
-          this.currentUserSource.next(user);
+         this.setCurrentUSer(user)
         }
       })
     );
@@ -31,8 +30,7 @@ export class AccountService {
     return this.http.post<User>(this.baseUrl + 'account/register',model).pipe(
       map(user => {
         if(user){
-          localStorage.setItem('user',JSON.stringify(user));
-          this.currentUserSource.next(user);
+         this.setCurrentUSer(user);
         }
         return user;
       })
@@ -40,6 +38,7 @@ export class AccountService {
   }
 
   setCurrentUSer(user: User) {
+    localStorage.setItem('user',JSON.stringify(user));
     this.currentUserSource.next(user);
   }
 
